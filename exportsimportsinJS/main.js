cdf594182650d951616343baf18fac03d7b2cd50
Rahul Shah <rahulnshah3012@gmail.com>
2022-06-13 23:11:43 -0400

    moved necessary things into this commit

--- exportsimportsinJS/main.js
+++ exportsimportsinJS/main.js
@@ -1,4 +1,4 @@
-const helpers = require("./helpers"); // if we do not use a relative path, Node will assume you're referencing a built-in module or a package. When you are using your own modules, always use relative paths.
+const helpers = require("./helpers"); //  if we do not use a relative path, Node will assume you're referencing a built-in module or a package. When you are using your own modules, always use relative paths.
 const moreHelpers = require("./moreHelpers");
 const evenMoreHelpers = require("./evenMoreHelpers");
 helpers.sayHi();
