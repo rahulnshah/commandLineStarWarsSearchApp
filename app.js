d5815ea839c4cc24a84adf0c9cb29657a4db3c16
Rahul Shah <rahulnshah3012@gmail.com>
2022-07-15 19:12:40 -0400

    leaderboard finisihed

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ if(process.argv.length >= 3 )
                 if(data.indexOf(result.name) < 0)
                 {
                   // add it to the unique list of names 
-                  let uniqueStr = result.name + "\n";
+                  let uniqueStr = result.name;
                   //save to a text file called star_wars_character_names.txt
 
                   /*
                   when appendFile is not nested into readFile method, the unique string is empty after looping through a 
                   list of returned names from the GET req. made to search endpoint of the SWAPI. 
                   */
-                  fs.appendFile("star_wars_characters.txt", uniqueStr, "utf-8", function(err){
+                  fs.appendFile("star_wars_characters.txt", `${uniqueStr}\n`, "utf-8", function(err){
                     if (err) {
                       console.log(err);
                       return; 
@@ -41,6 +41,27 @@ if(process.argv.length >= 3 )
                 }
               });
             }
+            // append the search word and the frequency to another text file 
+            fs.readFile("star_wars_characters.csv", "utf8", (err, data) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+              if(data.indexOf(`\"${searchKeyWord}\"`) < 0)
+              {
+                // add it to the unique list of names 
+                let uniqueStr = searchKeyWord;
+                
+                fs.appendFile("star_wars_characters.csv", `\"${uniqueStr}\",${json.count}\n`, "utf-8", function(err){
+                  if (err) {
+                    console.log(err);
+                    return; 
+                  }
+                  // also attach the searchkeyword to the result if not already there 
+                  console.log("Data is appended to csv file successfully.");
+                });
+              }
+            });
           }
           else
           {
@@ -51,7 +72,23 @@ if(process.argv.length >= 3 )
   }
   else
   {
-    console.log("Leaderboard:\n");
+    console.log("Leaderboard:");
+    fs.readFile("star_wars_characters.csv", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      let leaderboard = []; // list of objects 
+      let allLines = data.split("\n").slice(0,data.split("\n").length - 1);
+      
+      allLines.forEach((line) => {
+        let a = {}
+        a.keyword = line.split(",")[0];
+        a.timesAppeared = parseInt(line.split(",")[1]);
+        leaderboard.push(a);
+      });
+      console.table(leaderboard);
+    });
   }
 }
 else
